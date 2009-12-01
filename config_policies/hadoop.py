1c659bf0689769fbb452577dbee7ca6036f860e7
orbitz <orbitz@gmail.com>
2009-12-01 16:45:59 +0000

    Added hadoop policy

---
+++ config_policies/hadoop.py
@@ -0,0 +1,19 @@
+##
+# Ensure hadoop exists and is installed
+
+# Just pull everything from policy
+from igs.config_manage.policy import *
+
+PKG_NAME = 'hadoop-0.20.1'
+PKG_DIR = '${opt.package_dir}/' + PKG_NAME
+
+
+fileExists(PKG_DIR + '/conf/core-site.xml.tmpl')
+fileExists(PKG_DIR + '/conf/hdfs-site.xml.tmpl')
+fileExists(PKG_DIR + '/conf/mapred-site.xml.tmpl')
+
+executeTemplate(PKG_DIR + '/conf/core-site.xml.tmpl')
+executeTemplate(PKG_DIR + '/conf/hdfs-site.xml.tmpl')
+executeTemplate(PKG_DIR + '/conf/mapred-site.xml.tmpl')
+
+installOptPkg(PKG_NAME)
