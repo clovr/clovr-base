9c0a555559de96cb0bba415640df95ecbd3a3f12
Cesar Arze <cesar.arze@gmail.com>
2009-12-18 20:00:01 +0000

    config policy file for bioinf tools

---
+++ config_policies/bioinf.py
@@ -0,0 +1,13 @@
+##
+# Ensure bioinformatics tools exists and are installed
+
+# Just pull everything from policy
+from igs.config_manage.policy import *
+
+PKG_NAME = 'bioinf-v1r2b1'
+PKG_DIR = '${opt.package_dir}/' + PKG_NAME
+
+ensureOptPkg(PKG_NAME)
+run('mv /usr/local/bioinf /usr/local/bioinf.bak')
+run('ln -s %s /usr/local/bioinf' % (PKG_DIR + PKG_NAME))
+
