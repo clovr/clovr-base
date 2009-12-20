7d3abace19ae8c42a326acf1a66257465ee95583
orbitz <orbitz@gmail.com>
2009-12-20 04:18:59 +0000

    Turned bioinf off for right now, waiting for it to get commited to opt-packages

--- config_policies/bioinf.py
+++ config_policies/bioinf.py
@@ -7,6 +7,11 @@ from igs.config_manage.policy import *
 PKG_NAME = 'bioinf-v1r2b1'
 PKG_DIR = '${opt.package_dir}/' + PKG_NAME
 
+##
+# Turning this off for right now
+import sys
+sys.exit(0)
+
 ensureOptPkg(PKG_NAME)
 run('mv /usr/local/bioinf /usr/local/bioinf.bak')
 run('ln -s %s /usr/local/bioinf' % (PKG_DIR + PKG_NAME))
