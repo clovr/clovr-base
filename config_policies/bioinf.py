3fecabd8176ac271950db9fa39adde5603274a23
orbitz <orbitz@gmail.com>
2010-01-04 20:12:27 +0000

    Turning bioinf script back on

--- config_policies/bioinf.py
+++ config_policies/bioinf.py
@@ -4,15 +4,12 @@
 # Just pull everything from policy
 from igs.config_manage.policy import *
 
-PKG_NAME = 'bioinf-v1r2b1'
+PKG_NAME = 'bioinf-v1r4b1'
 PKG_DIR = '${opt.package_dir}/' + PKG_NAME
 
-##
-# Turning this off for right now
-import sys
-sys.exit(0)
-
 ensureOptPkg(PKG_NAME)
+##
+# we don't want to kill this now, so we'll back it up and then link our package there
 run('mv /usr/local/bioinf /usr/local/bioinf.bak')
 run('ln -s %s /usr/local/bioinf' % (PKG_DIR + PKG_NAME))
 
