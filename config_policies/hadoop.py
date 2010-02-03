c09233847ebd48deeb6b1e7fab7cc6d0fff8d77b
orbitz <orbitz@gmail.com>
2010-02-03 14:46:09 +0000

    Adding mongodb, restructuring how stow/opt-packages are installed

--- config_policies/hadoop.py
+++ config_policies/hadoop.py
@@ -15,5 +15,3 @@ fileExists(PKG_DIR + '/conf/mapred-site.xml.tmpl')
 executeTemplate(PKG_DIR + '/conf/core-site.xml.tmpl')
 executeTemplate(PKG_DIR + '/conf/hdfs-site.xml.tmpl')
 executeTemplate(PKG_DIR + '/conf/mapred-site.xml.tmpl')
-
-installOptPkg(PKG_NAME)
--- config_policies/tophat.py
+++
@@ -1,9 +0,0 @@
-##
-
-# Just pull everything from policy
-from igs.config_manage.policy import *
-
-PKG_NAME = 'tophat-1.0.12'
-
-ensurePkg(PKG_NAME)
-installPkg(PKG_NAME)
