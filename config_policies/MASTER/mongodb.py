f997d754e9ef4127bdec199e70a333cfdcc78738
orbitz <orbitz@gmail.com>
2010-02-05 16:02:00 +0000

    Temporary fix to mongodb, it's not starting right

--- config_policies/MASTER/mongodb.py
+++ config_policies/MASTER/mongodb.py
@@ -1,7 +1,10 @@
 ##
+import os
 # Just pull everything from policy
 from igs.config_manage.policy import *
 
 
 dirExists('/opt/db/mongo')
-run('mongod --dbpath=/opt/db/mongo --logpath=/var/log/mongodb.log --fork &')
+##
+# For some reason 'run' does not handle this properly so I'm just using os.system for now
+os.system('mongod --dbpath=/opt/db/mongo --logpath=/var/log/mongodb.log --fork')
