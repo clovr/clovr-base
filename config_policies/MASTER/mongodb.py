8e6edc7a19a0773217fe1367e709b442ce56c110
orbitz <orbitz@gmail.com>
2010-02-05 16:36:31 +0000

    Fixed mongod's backgrounding

--- config_policies/MASTER/mongodb.py
+++ config_policies/MASTER/mongodb.py
@@ -5,6 +5,4 @@ from igs.config_manage.policy import *
 
 
 dirExists('/opt/db/mongo')
-##
-# For some reason 'run' does not handle this properly so I'm just using os.system for now
-os.system('mongod --dbpath=/opt/db/mongo --logpath=/var/log/mongodb.log --fork')
+run('mongod --dbpath=/opt/db/mongo --logpath=/var/log/mongodb.log --fork > /dev/null 2>&1')
