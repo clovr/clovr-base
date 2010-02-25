d8e9afc5e6688d0ee1df93cff81e5910d1e2b2c5
orbitz <orbitz@gmail.com>
2010-02-25 14:37:54 +0000

    Adding a local cluster on mongo startup

--- config_policies/MASTER/mongodb.py
+++ config_policies/MASTER/mongodb.py
@@ -1,12 +1,45 @@
+import time
 ##
-import os
 # Just pull everything from policy
 from igs.config_manage.policy import *
+
+##
+# We are going to add the 'local' cluster to Mongo on start up
+# It might be possible that this should be moved to another script
+# but this will do for right now
+from igs.utils.config import configFromStream
+
+from vappio.cluster.persis_mongo import dump
+from vappio.cluster.control import Cluster
+
+##
+# this is temporary, going to need to replace this with some sort of 'local' implementation
+# even if it is just a dummy implemetnation
+from vappio import ec2
+
+
 
 def startup():
     dirExists('/opt/db/mongo')
     run('mongod --dbpath=/opt/db/mongo --logpath=/var/log/mongodb.log --fork > /dev/null 2>&1')
-
+    ##
+    # let mongo come up
+    time.sleep(1)
+    cluster = Cluster('local', ec2.control, configFromStream(open('/tmp/machine.conf')))
+    cluster.setMaster(ec2.control.Instance(instanceId='local',
+                                           amiId=None,
+                                           pubDns=cluster.config('MASTER_IP'),
+                                           privDns=cluster.config('MASTER_IP'),
+                                           state='running',
+                                           key=None,
+                                           index=None,
+                                           instanceType=None,
+                                           launch=None,
+                                           availabilityZone=None,
+                                           monitor=None))
+    dump(cluster)
+                                           
+    
 def shutdown():
     run('killall mongod')
     
