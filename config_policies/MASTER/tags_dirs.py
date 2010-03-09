3f66dbf538fe9727d122f17e5b790c19e1a33a38
orbitz <orbitz@gmail.com>
2010-03-09 16:28:03 +0000

    Typo

--- config_policies/MASTER/tags_dirs.py
+++ config_policies/MASTER/tags_dirs.py
@@ -6,7 +6,7 @@ from igs.config_manage.policy import *
 def startup():
     dirExists('${dirs.upload_dir}')
     dirExists('${dirs.tag_dir}')
-    dirOwner('${dirs.upload_dir', 'www-data')
+    dirOwner('${dirs.upload_dir}', 'www-data')
     dirOwner('${dirs.tag_dir}', 'www-data')
 
 def shutdown():
