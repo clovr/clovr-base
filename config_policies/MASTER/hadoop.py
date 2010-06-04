e57bfca20fcc1d4bba41a5ee7c2c7fe8a256c8cf
orbitz <orbitz@gmail.com>
2010-06-04 15:34:44 +0000

    Making master a data node
    Former-commit-id: dac0a63ee54f7a016a35aae6c7b30de45c2c8772

--- config_policies/MASTER/hadoop.py
+++ config_policies/MASTER/hadoop.py
@@ -3,6 +3,7 @@
 # This assumes that hadoop config files have already been created and now it is just
 # a matter of running the right programs
 # It assumes it is in ${opt.base_dir}/hadoop
+import time
 
 # Just pull everything from policy
 from igs.config_manage.policy import *
@@ -14,9 +15,14 @@ def startup():
     # We do not want to format it if it's already been created
     run('yes N | ' + PKG_DIR + '/bin/hadoop namenode -format', ignoreError=True)
     run(PKG_DIR + '/bin/hadoop-daemon.sh start namenode')
+    time.sleep(5)
     run(PKG_DIR + '/bin/hadoop-daemon.sh start jobtracker')
-
+    run(PKG_DIR + '/bin/hadoop-daemon.sh start datanode')
+    run(PKG_DIR + '/bin/hadoop-daemon.sh start tasktracker')
 
 def shutdown():
-    run(PKG_DIR + '/bin/hadoop-daemon.sh stop namenode')
     run(PKG_DIR + '/bin/hadoop-daemon.sh stop jobtracker')    
+    run(PKG_DIR + '/bin/hadoop-daemon.sh stop datanode')
+    run(PKG_DIR + '/bin/hadoop-daemon.sh stop tasktracker')
+    run(PKG_DIR + '/bin/hadoop-daemon.sh stop namenode')
+
