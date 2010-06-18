6d8c03d9dad4002a0d0960f5666b215acc249839
Tim Bogatchev <tbogatchev@fake.com>
2010-06-18 18:06:40 +0000

    fixed ergatisparse.pl
    Former-commit-id: e683f4b9bf0ea21892e86a918daa6220f0ed889a

--- hudson/hudson-scripts/ergatisparse.pl
+++ hudson/hudson-scripts/ergatisparse.pl
@@ -9,14 +9,13 @@ while ( my $line = <> ) {
     my $filename = basename($line); 
     if($filename =~ /([^\.]+)/) {
 	my $component = "$1".".config";
-	if (! -e `/opt/ergatis/docs/$component`)
+	if (! -e "/opt/ergatis/docs/$component")
 	{
 	    print "$line cannot be found in /opt/ergatis/docs/ \n";
 	    exit 1;
 	}
 	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | sort`;
 	my $second_file = `grep '\$;' /opt/ergatis/docs/$component | cut -f 1 -d '=' | sort`;
-	print "$line\n";
 	if( $first_file == $second_file) { 
 	    exit 0;
 	}
