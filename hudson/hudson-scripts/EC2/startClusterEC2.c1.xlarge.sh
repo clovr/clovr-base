786e676148b77ea1b48ced10459611328f9436e0
Tim Bogatchev <tbogatchev@fake.com>
2010-06-25 18:17:22 +0000

    EC2 tests
    Former-commit-id: 9bf351d477b1207cc86dfae32542be347d18b1a2

---
+++ hudson/hudson-scripts/EC2/startClusterEC2.c1.xlarge.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+source /opt/vappio-scripts/clovrEnv.sh
+
+ec2-describe-instances
+
+if [ "$?" != "0" ] ; then
+echo "EC2 isn't working"
+exit 0
+fi
+
+cp /opt/vappio-conf/clovr.conf /opt/vappio-conf/temp.conf
+sed -e "s/master_type=m1.large/master_type=c1.xlarge/g" /opt/vappio-conf/temp.conf
+
+startDate=`date +"%s"`
+
+startCluster.py --conf-name=temp.conf --name=ec2_test --num=0 --ctype=ec2 -b
+success=$?
+
+endDate=`date +"%s"`
+finalDate=$((endDate-startDate))
+
+terminateCluster.py --name=ec2_test
+
+echo "Time elapsed: $finalDate seconds"
+rm /opt/vappio-conf/temp.conf
+
+exit $success
--- hudson/hudson-scripts/listDependencies.sh
+++ hudson/hudson-scripts/listDependencies.sh
@@ -6,6 +6,12 @@ do
     temp=`ls /opt/clovr_pipelines/workflow/project_saved_templates/$each | grep .config`
     for config in $temp
     do
-	cat /opt/clovr_pipelines/workflow/project_saved_templates/$each/$config | grep -iE "(TEMPLATE_XML|ITERATOR1_XML)"
+	grep -E "(^\\$;TEMPLATE_XML|^\\$;ITERATOR1_XML)" /opt/clovr_pipelines/workflow/project_saved_templates/$each/$config > /tmp/temp.txt
+	component=`echo $config | perl -ne 'if(/([^\.]+)/){print "$1\n" }'`
+	while read line     
+	do
+	    echo $line | sed -e "s/$;TEMPLATE_XML$; = $;DOCS_DIR$;/\/opt\/ergatis\/docs/g" |  sed -e "s/$;TEMPLATE_XML$;=$;DOCS_DIR$;/\/opt\/ergatis\/docs/g" |  sed -e "s/$;ITERATOR1_XML$;=$;DOCS_DIR$;/\/opt\/ergatis\/docs/g" |  sed -e "s/$;ITERATOR1_XML$; = $;DOCS_DIR$;/\/opt\/ergatis\/docs/g"
+	done < /tmp/temp.txt
+	
     done
 done
