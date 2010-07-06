8efc3cd9dfb38c2ed8a3211ed523a1b487da3103
orbitz <orbitz@gmail.com>
2010-07-06 19:11:26 +0000

    Removing bootStrapKeys.sh call, not needed
    Former-commit-id: 1db4de7d894b77aef7d8a069e447bc53ed4e1f7c

--- hudson/hudson-scripts/total_metagenomics_stub.sh
+++ hudson/hudson-scripts/total_metagenomics_stub.sh
@@ -8,8 +8,6 @@ cp /opt/hudson/total_metagenomics_stub.config /tmp/pipeline.conf
 
 sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
 
-bootStrapKeys.py
-
 
 TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name microbe-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
 
