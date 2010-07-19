2b2796faf3ba7cf67a616e470df87f7de97f2afb
Sam Angiuoli <angiuoli@fake.com>
2010-07-19 19:20:12 +0000

    removing this copy of globus. version in /opt/globus-5.0.0 is working on image
    Former-commit-id: ecba175da7cce20a655c706e7514fe334d3bf9d3

--- opt-packages/globus-5.0.0/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/opt/opt-packages/globus-5.0.0/lib"
-XML2_LIBS="-lxml2    -lm  "
-XML2_INCLUDEDIR="-I/opt/opt-packages/globus-5.0.0/include/libxml2"
-MODULE_VERSION="xml2-1.8"
-
