89db420f4d6fbc8adff891c85bf4155ae1216afd
Sam Angiuoli <angiuoli@fake.com>
2010-09-02 22:49:03 +0000

    Former-commit-id: e40240dc1540265994e9c0b7e076aea8c48d20ab

--- hudson/hudson-scripts/ergatisparse.pl
+++ hudson/hudson-scripts/ergatisparse.pl
@@ -2,7 +2,7 @@
 
 use strict;
 use File::Basename; 
-
+my $error=0;
 while ( my $line = <> ) {
     chomp($line);
 
@@ -14,17 +14,22 @@ while ( my $line = <> ) {
 	    print "$line cannot be found in /opt/ergatis/docs/ \n";
 	    exit 1;
 	}
-	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | sort`;
-	my $second_file = `grep '\$;' /opt/ergatis/docs/$component | cut -f 1 -d '=' | sort`;
-	if( $first_file == $second_file) { 
-	    exit 0;
+	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | perl -ne 's/\\\$\;//g;print' | sort -u | perl -ne 's/\s+//g;print' > /tmp/file1`;
+	my $second_file = `grep '\$;' /opt/ergatis/docs/$component | cut -f 1 -d '=' | perl -ne 's/\\\$\;//g;print' | perl -ne 's/\s+//g;print' | sort -u > /tmp/file2`;
+	my $diff = `diff --ignore-all-space --side-by-side /tmp/file1 /tmp/file2`;
+	if( $? eq 0) { 
+		print "$component OK in $filename\n";
 	}
 	else  
 	{
-	    exit 1;
+	    print "ERROR $component NOT OK in $line\n";
+		print "$diff\n";
+	    $error=1;
 	}
 	
     }
 }
- 
+if($error){
+	exit 1;
+} 
          
---
+++ hudson/hudson-scripts/ulimit.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+
+print "$ARGV[0]\n";
+foreach $i (1..$ARGV[0]) {
+
+$FH="FH${i}";
+
+open ($FH,'>',"/tmp/Test${i}.log") || die "$!";
+
+print $FH "$i\n";
+
+} 
