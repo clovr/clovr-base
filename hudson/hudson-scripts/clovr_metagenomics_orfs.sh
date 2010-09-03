e7d52cd679d9bdafc780e7fea9ec98e66670df3a
James White <jamesrwhite@fake.com>
2010-09-03 21:21:27 +0000

    added hudson tests for new metagenomics pipelines
    Former-commit-id: d68da9e8a16578c581140e224bcdfb7e6698e57a

---
+++ hudson/hudson-scripts/clovr_metagenomics_orfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+source /root/clovrEnv.sh
+
+DATE=`date +"%m-%d-%Y-%T"`
+
+cp /opt/hudson/clovr_metagenomics_orfs.config /tmp/pipeline.conf
+
+sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
+
+
+TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_metagenomics_orfs-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
+
+if [ "$?" == "1" ]; then
+    echo "runPipeline.py failed to run"
+    exit 1
+fi
+
+taskStatus.py --name local --exit-code --block $TASK_NAME
+
+exit $?
+
+
+
+
