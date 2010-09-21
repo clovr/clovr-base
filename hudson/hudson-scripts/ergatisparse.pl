49c11affc9412279d63c2b4d0538bff7bfaabe3c
Sam Angiuoli <angiuoli@fake.com>
2010-09-21 13:56:45 +0000

    Former-commit-id: db025792db0a8ceb2f4c773bd8d3e64be85b837a

--- hudson/hudson-scripts/ergatisparse.pl
+++ hudson/hudson-scripts/ergatisparse.pl
@@ -3,26 +3,27 @@
 use strict;
 use File::Basename; 
 my $error=0;
+my $ergatisdocs="/opt/ergatis/docs";
 while ( my $line = <> ) {
     chomp($line);
 
     my $filename = basename($line); 
     if($filename =~ /([^\.]+)/) {
 	my $component = "$1".".config";
-	if (! -e "/opt/ergatis/docs/$component")
+	if (! -e "$ergatisdocs/$component")
 	{
-	    print "$line cannot be found in /opt/ergatis/docs/ \n";
+	    print "$line cannot be found in $ergatisdocs/ \n";
 	    $error=1;
 	}
 	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | perl -ne 's/\\\$\;//g;print' | sort -u | perl -ne 's/\s+//g;print' > /tmp/file1`;
-	my $second_file = `grep '\$;' /opt/ergatis/docs/$component | cut -f 1 -d '='  | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | perl -ne 's/\\\$\;//g;print' | perl -ne 's/\s+//g;print' | sort -u > /tmp/file2`;
+	my $second_file = `grep '\$;' $ergatisdocs/$component | cut -f 1 -d '='  | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | perl -ne 's/\\\$\;//g;print' | perl -ne 's/\s+//g;print' | sort -u > /tmp/file2`;
 	my $diff = `diff --ignore-all-space --side-by-side /tmp/file2 /tmp/file1`;
 	if( $? eq 0) { 
-		print "$component OK in $filename\n";
+		print "Component $ergatisdocs/$component MATCHES pipeline $filename\n";
 	}
 	else  
 	{
-	    print "ERROR $component NOT OK in $line\n";
+	    print "ERROR component $ergatisdocs/$component DOES NOT MATCH pipeline $line\n";
 		print "$diff\n";
 	    $error=1;
 	}
