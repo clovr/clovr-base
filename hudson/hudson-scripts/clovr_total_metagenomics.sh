f020863a0b930b9d30f715031b5eaddfbb9cce63
Cesar Arze <cesar.arze@gmail.com>
2010-09-24 13:55:53 +0000

    Renaming total metagenomics test script names and reconfiguring to account for tagging outside of pipeline
    Former-commit-id: 8efd3e60eb015db16da5c9696a8bf854dbf858a2

--- hudson/hudson-scripts/clovr_total_metagenomics.sh
+++ hudson/hudson-scripts/clovr_total_metagenomics.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-set -e
+set -e 
 source /root/clovrEnv.sh
 
 DATE=`date +"%m-%d-%Y-%T"`
 
-cp /opt/hudson/total_metagenomics_stub.config /tmp/pipeline.conf
+# Need to tag our data prior to kicking off the pipeline 
+vp-add-dataset --tag-name=clovr_total_metagenomics_hudson /opt/hudson/MOMSP_454Reads_Region1_subset2.fasta -o
 
+cp /opt/hudson/clovr_total_metagenomics.config /tmp/pipeline.conf
 sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
-
 
 TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name microbe-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
 
@@ -19,7 +20,3 @@ fi
 taskStatus.py --name local --exit-code --block $TASK_NAME
 
 exit $?
-
-
-
-
