63a238d56cbd35cc04dbacaab73df9182d77c9c8
Sam Angiuoli <angiuoli@fake.com>
2010-09-25 20:01:56 +0000

    Former-commit-id: cf37488b2c82953f14a1324a640e2694ea910cb2

--- hudson/hudson-scripts/launchAll.sh
+++ hudson/hudson-scripts/launchAll.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
-find /var/lib/hudson/jobs/ -maxdepth 1 -name "[[:digit:]]*" -printf "%f\n" | sort -n > /tmp/jobs.txt
+find /var/lib/hudson/jobs/ -maxdepth 1 -name "[[:digit:]]*" -printf "%f\n" | sort -n | grep -v -i -P 'pipeline$' > /tmp/jobs.txt
 
 ip=`hostname`
 #ip=${ip:6}
 #ip=`echo $ip | sed -e 's/-/./g'` 
 
+#Launch non pipeline jobs first
 while read line 
 do  
      
@@ -15,3 +16,4 @@ do
 	curl `echo "http://$ip:8888/job/$line/build" | sed -e 's/ /\%20/g'`
     fi
 done < /tmp/jobs.txt
+
---
+++ hudson/hudson-scripts/launchAllPipelines.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+find /var/lib/hudson/jobs/ -maxdepth 1 -name "[[:digit:]]*" -printf "%f\n" | sort -n | grep -v -i -P 'pipeline$' > /tmp/pipelinejobs.txt
+
+ip=`hostname`
+#ip=${ip:6}
+#ip=`echo $ip | sed -e 's/-/./g'` 
+
+#Launch non pipeline jobs first
+while read line 
+do  
+    curl `echo "http://$ip:8888/job/$line/build" | sed -e 's/ /\%20/g'`
+done < /tmp/pipelinejobs.txt
+
