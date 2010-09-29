b4c62b6ffd4f43b82a93047bbf196b048c36985b
orbitz <orbitz@gmail.com>
2010-09-29 18:41:32 +0000

    Modified clovr_16S for new style
    Former-commit-id: 90c4d82be6fb6ade4ddb3ae0ec91a0b8a3349ac5

--- hudson/hudson-scripts/clovr_16S_multi.sh
+++ hudson/hudson-scripts/clovr_16S_multi.sh
@@ -2,14 +2,18 @@
 set -e
 source /root/clovrEnv.sh
 
+vp-add-dataset -o --tag-name=clovr_16S_multi_input /opt/hudson/16S_data/A.fasta /opt/hudson/16S_data/B.fasta /opt/hudson/16S_data/C.fasta /opt/hudson/16S_data/D.fasta
+vp-add-dataset -o --tag-name=clovr_16S_multi_mapping /opt/hudson/16S_data/IGS.multi.meta
+
 DATE=`date +"%m-%d-%Y-%T"`
 
-cp /opt/hudson/clovr_16S_multi.config /tmp/pipeline.conf
+vp-describe-protocols --config-from-protocol=clovr_16S \
+    -c input.FASTA_TAG=clovr_16S_multi_input \
+    -c input.MAPPING_TAG=clovr_16S_multi_mapping \
+    -c input.PIPELINE_NAME=clovr_16S_multi_${DATE} \
+    > /tmp/pipeline.conf
 
-sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
-
-
-TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_16S-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
+TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_16S_multi_$$ --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
 
 if [ "$?" == "1" ]; then
     echo "runPipeline.py failed to run"
--- hudson/hudson-scripts/clovr_16S_single.sh
+++ hudson/hudson-scripts/clovr_16S_single.sh
@@ -2,14 +2,18 @@
 set -e
 source /root/clovrEnv.sh
 
+vp-add-dataset -o --tag-name=clovr_16S_single_input /opt/hudson/16S_data/AMP_Lung.small.fasta
+vp-add-dataset -o --tag-name=clovr_16S_single_mapping /opt/hudson/16S_data/IGS.qmap
+
 DATE=`date +"%m-%d-%Y-%T"`
 
-cp /opt/hudson/clovr_16S_single.config /tmp/pipeline.conf
+vp-describe-protocols --config-from-protocol=clovr_16S \
+    -c input.FASTA_TAG=clovr_16S_single_input \
+    -c input.MAPPING_TAG=clovr_16S_single_mapping \
+    -c input.PIPELINE_NAME=clovr_16S_single_${DATE} \
+    > /tmp/pipeline.conf
 
-sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
-
-
-TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_16S-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
+TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_16S_single_$$ --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
 
 if [ "$?" == "1" ]; then
     echo "runPipeline.py failed to run"
