215d3cf8607d794f0021494bbc179635a98483cf
orbitz <orbitz@gmail.com>
2010-09-29 15:37:57 +0000

    Updated clovr_sleep to new style
    Former-commit-id: 57692de4ae40f26d47531c64bb40cf1897d23a63

--- hudson/hudson-scripts/clovr_search.sh
+++ hudson/hudson-scripts/clovr_search.sh
@@ -9,7 +9,12 @@ vp-add-dataset -o --tag-name=NC_000964_blastpdb /opt/hudson/NC_000964/NC_000964.
 
 DATE=`date +"%m-%d-%Y-%T"`
 
-vp-describe-protocols --config-from-protocol=clovr_search -c input.INPUT_TAG=NC_000964_peps -c input.REF_DB_TAG=NC_000964_blastpdb -c input.PIPELINE_NAME=${DATE} -c misc.PROGRAM=blastp > /tmp/pipeline.conf
+vp-describe-protocols --config-from-protocol=clovr_search \
+    -c input.INPUT_TAG=NC_000964_peps \
+    -c input.REF_DB_TAG=NC_000964_blastpdb \
+    -c input.PIPELINE_NAME=clovr_search_${DATE} \
+    -c misc.PROGRAM=blastp \
+    > /tmp/pipeline.conf
 
 TASK_NAME=`runPipeline.py --print-task-name --pipeline clovr_wrapper --name local -n blastall$$ -- --CONFIG_FILE=/tmp/pipeline.conf`
 
--- hudson/hudson-scripts/clovr_sleep.sh
+++ hudson/hudson-scripts/clovr_sleep.sh
@@ -4,9 +4,9 @@ source /root/clovrEnv.sh
 
 DATE=`date +"%m-%d-%Y-%T"`
 
-cp /opt/hudson/clovr_sleep.config /tmp/pipeline.conf
-
-sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
+vp-describe-protocols --config-from-protocol=clovr_sleep \
+    -c input.PIPELINE_NAME=clovr_sleep_${DATE} \
+    > /tmp/pipeline.conf
 
 TASK_NAME=`runPipeline.py --print-task-name --pipeline clovr_wrapper --name local -n clovrsleep -- --CONFIG_FILE=/tmp/pipeline.conf`
 
