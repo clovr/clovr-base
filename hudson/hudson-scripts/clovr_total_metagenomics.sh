18480799e01d0220a7e979aea5e0e72a7daa589e
orbitz <orbitz@gmail.com>
2010-09-29 14:40:16 +0000

    Commiting clovr_total_metagenomics to new config file style
    Former-commit-id: eebc3f6e4b9684eb49ac2f0d9af3b303e24b7eec

--- hudson/hudson-scripts/clovr_total_metagenomics.sh
+++ hudson/hudson-scripts/clovr_total_metagenomics.sh
@@ -7,8 +7,7 @@ DATE=`date +"%m-%d-%Y-%T"`
 # Need to tag our data prior to kicking off the pipeline 
 vp-add-dataset --tag-name=clovr_total_metagenomics_hudson /opt/hudson/MOMSP_454Reads_Region1_subset2.fasta -o
 
-cp /opt/hudson/clovr_total_metagenomics.config /tmp/pipeline.conf
-sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
+vp-describe-protocols --config-from-protocol=clovr_total_metagenomics -c input.INPUT_TAG=clovr_total_metagenomics_hudson -c input.PIPELINE_NAME=${DATE} > /tmp/pipeline.conf
 
 TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name microbe-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
 
