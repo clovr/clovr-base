22681c081efcdb24ee477461f99132c45fbc1e9f
Mahesh Vangala <vangalamaheshh@gmail.com>
2010-09-30 19:07:57 +0000

    added the pangenome related files for the hudson test
    Former-commit-id: 1c9c695c9b228229d688ef176f8f0f9edf5b71ba

---
+++ hudson/hudson-scripts/clovr_pangenome.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+source /root/clovrEnv.sh
+
+DATE=`date + "%m-%d-%y-%T"`
+
+cp /opt/hudson/clovr_pangenome.config /tmp/pipeline.conf
+
+sed -i -e "s/\${DATE}/$DATE/" /tmp/pipeline.conf
+
+tagData.py --tag-name=bifidobacter_genbank_tag -o -r /opt/hudson/pangenome_data/bifidobacter_genbank_files
+tagData.py --tag-name=bifidobacter_map_tag -o -r /opt/hudson/pangenome_data/bifidobacter_map_files
+
+TASK_NAME=`runPipeline.py --name local --print-task-name --pipeline-name clovr_comparative-$DATE --pipeline=clovr_wrapper -- --CONFIG_FILE=/tmp/pipeline.conf`
+if [ "$?" == "1" ]; then
+	echo "runPipeline.py failed to run"
+	exit 1
+fi
+
+taskStatus.py --name local --exit-code --block $TASK_NAME
+
+exit $?
