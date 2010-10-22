a5fd4fd66e797731e632755d430cf0f6926ce8a1
Sam Angiuoli <angiuoli@fake.com>
2010-10-22 21:56:34 +0000

    updated naming convention, added metadata
    Former-commit-id: 77904f05f2b9affe5255dbcf5fef4ed6b9f2971f

--- hudson/hudson-scripts/clovr_search.sh
+++ hudson/hudson-scripts/clovr_search.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-source /root/clovrEnv.sh
+source /opt/vappio-scripts/clovrEnv.sh
 
-vp-add-dataset -o --tag-name=NC_000964_peps /opt/hudson/NC_000964/NC_000964.faa
+vp-add-dataset -o --tag-name=NC_000964_peps -m format_type=aa_FASTA /opt/hudson/NC_000964/NC_000964.faa
 
-vp-add-dataset -o --tag-name=NC_000964_blastpdb /opt/hudson/NC_000964/NC_000964.faa*
-
+vp-add-dataset -o --tag-name=NC_000964_blastpdb -m format_type=aa_blastdb /opt/hudson/NC_000964/NC_000964.faa*
 
 DATE=`date +"%m-%d-%Y-%T"`
 
@@ -14,15 +13,15 @@ vp-describe-protocols --config-from-protocol=clovr_search \
     -c input.REF_DB_TAG=NC_000964_blastpdb \
     -c input.PIPELINE_NAME=clovr_search_${DATE} \
     -c misc.PROGRAM=blastp \
-    > /tmp/pipeline.conf
+    > /tmp/$$.pipeline.conf
 
-TASK_NAME=`runPipeline.py --print-task-name --pipeline clovr_wrapper --name local -n clovr_search_$$ -- --CONFIG_FILE=/tmp/pipeline.conf`
+TASK_NAME=`vp-run-pipeline --print-task-name --pipeline clovr_wrapper --name local -n clovr_search_$$ -- --CONFIG_FILE=/tmp/$$.pipeline.conf`
 
 if [ "$?" == "1" ]; then
-    echo "runPipeline.py failed to run"
+    echo "vp-run-pipeline failed to run"
     exit 1
 fi
 
-taskStatus.py --name local --exit-code --block $TASK_NAME
+vp-describe-task --name local --exit-code --block $TASK_NAME
 
 exit $?
