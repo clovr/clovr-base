9edd0bd9d5f9956ef58ef95f434a29acbeeca055
Sam Angiuoli <angiuoli@fake.com>
2010-10-26 21:38:07 +0000

    Former-commit-id: d59482378f340be1acc70e5848434dac589d7ef9

--- hudson/hudson-scripts/ergatisparse.pl
+++ hudson/hudson-scripts/ergatisparse.pl
@@ -15,8 +15,8 @@ while ( my $line = <> ) {
 	    print "$line cannot be found in $ergatisdocs/ \n";
 	    $error=1;
 	}
-	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | grep -v STAGEDATA | grep -v PREPROC | grep -v POSTPROC | grep -v COMPONENT_TWIG_XML | perl -ne 's/\\\$\;//g;print' | sort -u | perl -ne 's/\s+//g;print' > /tmp/file1`;
-	my $second_file = `grep '\$;' $ergatisdocs/$component | cut -f 1 -d '='  | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | grep -v STAGEDATA | grep -v PREPROC | grep -v POSTPROC | grep -v COMPONENT_TWIG_XML | perl -ne 's/\\\$\;//g;print' | perl -ne 's/\s+//g;print' | sort -u > /tmp/file2`;
+	my $first_file = `grep '\$;' $line | cut -f 1 -d '=' | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | grep -v STAGEDATA | grep -v PREPROC | grep -v POSTPROC | grep STAGING_DIR | grep -v COMPONENT_TWIG_XML | perl -ne 's/\\\$\;//g;print' | sort -u | perl -ne 's/\s+//g;print' > /tmp/file1`;
+	my $second_file = `grep '\$;' $ergatisdocs/$component | cut -f 1 -d '='  | grep -v -P '^\;' | grep -v SKIP_WF_COMMAND | grep -v STAGEDATA | grep -v PREPROC | grep -v POSTPROC | grep STAGING_DIR | grep -v COMPONENT_TWIG_XML | perl -ne 's/\\\$\;//g;print' | perl -ne 's/\s+//g;print' | sort -u > /tmp/file2`;
 	my $diff = `diff --ignore-all-space --side-by-side /tmp/file2 /tmp/file1`;
 	if( $? eq 0) {
 		print "OK. Component $ergatisdocs/$component MATCHES pipeline $line\n";
