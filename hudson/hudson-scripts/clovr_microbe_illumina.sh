28d5ed058a31e734ab0f237e4978639c2f571ac2
Sam Angiuoli <angiuoli@fake.com>
2010-10-31 13:04:07 +0000

    attempting fix of failed test
    Former-commit-id: 26bce43181e0f9f2bd38efbe9000f79e58a520e9

--- hudson/hudson-scripts/clovr_microbe_illumina.sh
+++ hudson/hudson-scripts/clovr_microbe_illumina.sh
@@ -7,15 +7,15 @@ DATE=`date +"%m-%d-%Y-%T"`
 vp-add-dataset --tag-name=clovr_microbe_illumina_tag /opt/hudson/illumina_data/partial_reads_1.fastq /opt/hudson/illumina_data/partial_reads_2.fastq -o
 
 vp-describe-protocols --config-from-protocol=clovr_microbe_illumina \
-    -c input.SHORT_PAIRED_TAGS=clovr_microbe_illumina_tag \
+    -c input.SHORT_PAIRED_FILES=clovr_microbe_illumina_tag \
     -c input.PIPELINE_NAME=clovr_microbe_illumina_${DATE} \
     > /tmp/$$.pipeline.conf
 
 
-TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name microbe_illumina-$DATE --pipeline=clovr_wrapper --pipeline-config=/tmp/$$.pipeline.conf`
+TASK_NAME=`vp-run-pipeline --name local --print-task-name --pipeline-name microbe_illumina-$DATE --pipeline=clovr_wrapper --pipeline-config=/tmp/$$.pipeline.conf`
 
 if [ "$?" == "1" ]; then
-    echo "runPipeline.py failed to run"
+    echo "vp-run-pipeline failed to run"
     exit 1
 fi
 
