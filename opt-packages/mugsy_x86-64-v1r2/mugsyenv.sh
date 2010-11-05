744df8e088315745c71f327c5dfa7288f50a0d6b
Mahesh Vangala <vangalamaheshh@gmail.com>
2010-11-05 17:47:06 +0000

    deleted mugsy version 1; added mugsy version 2
    Former-commit-id: 25395ee9f288f39e9a499e207e67bcd575e14c27

---
+++ opt-packages/mugsy_x86-64-v1r2/mugsyenv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export MUGSY_INSTALL=./mugsy_x86-64-v1r2
+export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping
+export PERL5LIB=$MUGSY_INSTALL/perllibs
+#For testing TBA
+#export PATH=$PATH:$MUGSY_INSTALL/../../multiz-tba/trunk/
+
