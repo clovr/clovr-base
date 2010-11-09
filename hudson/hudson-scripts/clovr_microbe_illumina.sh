0699b42dff5865ed5cf326d71b69c77a6bbd3aff
Sam Angiuoli <angiuoli@fake.com>
2010-11-09 16:46:07 +0000

    Former-commit-id: 9059fdabfff562cfb9e545b6c04580ff23824d1d

--- hudson/hudson-scripts/clovr_microbe_illumina.sh
+++ hudson/hudson-scripts/clovr_microbe_illumina.sh
@@ -4,11 +4,11 @@ source /opt/vappio-scripts/clovrEnv.sh
 
 DATE=`date +"%m-%d-%Y-%T"`
 
-vp-add-dataset --tag-name=clovr_microbe_illumina_tag /opt/hudson/illumina_data/partial_reads_1.fastq /opt/hudson/illumina_data/partial_reads_2.fastq -o
+vp-add-dataset -o --tag-name=clovr_microbe_illumina_tag /opt/hudson/illumina_data/partial_reads_1.fastq /opt/hudson/illumina_data/partial_reads_2.fastq 
 
 vp-describe-protocols --config-from-protocol=clovr_microbe_illumina \
     -c input.SHORT_PAIRED_TAG=clovr_microbe_illumina_tag \
-    -c input.PIPELINE_NAME=clovr_microbe_illumina_${DATE} \
+    -c input.PIPELINE_NAME=clovr_microbe_illumina-${DATE} \
     -c input.OUTPUT_PREFIX=test \
     -c input.ORGANISM="Genus species" \
     > /tmp/$$.pipeline.conf
