6836fdee9589eb12720bb99744da06b7e4cef8e1
Mahesh Vangala <vangalamaheshh@gmail.com>
2011-01-12 19:52:36 +0000

    changed default from 10 to 2.
    Former-commit-id: 49f14721df5322b052353ccebf8941cd57caefc4

--- hudson/hudson-scripts/add_hudson_executors.sh
+++ hudson/hudson-scripts/add_hudson_executors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NUM_EXECS="$1"
+export NUM_EXECS=2
 perl -pi -e 's/\<numExecutors\>\d+\<\/numExecutors\>/<numExecutors\>$ENV{NUM_EXECS}<\/numExecutors>/' /var/lib/hudson/config.xml
 curl -d "json=%7B%7D&Submit=Yes" "http://localhost:8888/restart"
 
