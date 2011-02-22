d92aca370b54b594427c977ac4c7d0226f352fba
orbitz <orbitz@gmail.com>
2011-02-22 17:02:40 +0000

    Adding start/stop ot ocamlmq
    Former-commit-id: eae4e58508efe8ef90f9d529e37fbcf5e01530ef

--- opt-packages/ocamlmq-1.1/ocamlmq_daemonize.sh
+++ opt-packages/ocamlmq-1.1/ocamlmq_daemonize.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+
+case "$1" in
+    start)
+	nohup /opt/ocamlmq/ocamlmq > /dev/null 2>&1 &
+	;;
+    stop)
+	kill `ps auxww | grep ocamlmq | grep -v grep | awk '{ print $2 }'`
+	;;
+    restart)
+	$0 stop
+	sleep 1
+	$0 start
+	;;
+    *)
+	echo "Unknown option"
+	exit 1
+	;;
+esac
+
+exit 0
+
+
 #!/bin/sh
 
-nohup /opt/ocamlmq/ocamlmq > /dev/null 2>&1 &
+
