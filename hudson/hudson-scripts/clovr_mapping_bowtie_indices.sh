b855aed176f5287fbf6aac1269028a42c314d129
Cesar Arze <cesar.arze@gmail.com>
2011-05-25 18:45:06 +0000

    Adding RNA seq hudson test
    Former-commit-id: d8dd84dd8715e7ff372882338cb417ba291503c3

--- hudson/hudson-scripts/clovr_mapping_bowtie_indices.sh
+++ hudson/hudson-scripts/clovr_mapping_bowtie_indices.sh
@@ -2,20 +2,25 @@
 set -e
 source /opt/vappio-scripts/clovrEnv.sh
 
-DATE=`date +"%m-%d-%Y-%T" | sed -e 's/:/_/g'`
+DATE=`date +"%m-%d-%Y-%T"`
 
 # Tag data that will be used in this pipeline
-vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_reference /opt/hudson/bowtie_index/NC_008253*
-vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_input_reads /opt/hudson/e_coli_1000.fq
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_paired_reads_1 /opt/hudson/rna_seq_data/test1.txt /opt/hudson/rna_seq_data/test2.txt
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_paired_reads_2 /opt/hudson/rna_seq_data/testb1.txt /opt/hudson/rna_seq_data/testb2.txt
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_bowtie_index /opt/hudson/rna_seq_data/bowtie_indices/*
 
 # Generate configuration file that will be used by the pipeline
 vp-describe-protocols --config-from-protocol=clovr_mapping_bowtie_indices \
-    -c input.REFERENCE_TAG=clovr_mapping_bowtie_indices_reference \
-    -c input.INPUT_READS_TAG=clovr_mapping_bowtie_indices_input_reads \
+    -c input.INPUT_PAIRED_TAG="clovr_mapping_bowtie_indices_paired_reads_1,clovr_mapping_bowtie_indices_paired_reads_2" \
+    -c input.REFERENCE_TAG=clovr_mapping_bowtie_indices_bowtie_index \
+    -c params.MAX_INSERT_SIZE=300 \
+    -c pipeline.PIPELINE_NAME=clovr_mapping_bowtie_indices_${DATE} \
+    -c cluster.CLUSTER_NAME=$1 \
+    -c cluster.CLUSTER_CREDENTIAL=$2 \
      > /tmp/$$.pipeline.conf
 
 # Run pipeline, block on checking status and verify exit code indicates a successful run
-TASK_NAME=`vp-run-pipeline --print-task-name --pipeline-config=/tmp/$$.pipeline.conf`
+TASK_NAME=`runPipeline.py --name local  --print-task-name --pipeline-name clovr_mapping_bowtie_indices_$$ --pipeline=clovr_wrapper --pipeline-config=/tmp/$$.pipeline.conf`
 
 if [ "$?" == "1" ]; then
     echo "runPipeline.py failed to run"
--- hudson/hudson-scripts/clovr_mapping_bowtie_noindices.sh
+++ hudson/hudson-scripts/clovr_mapping_bowtie_noindices.sh
@@ -2,25 +2,28 @@
 set -e
 source /opt/vappio-scripts/clovrEnv.sh
 
-DATE=`date +"%m-%d-%Y-%T" | sed -e 's/:/_/g'`
+DATE=`date +"%m-%d-%Y-%T"`
 
 # Tag data that will be used in this pipeline
-vp-add-dataset -o --tag-name=clovr_mapping_bowtie_noindices_reference /opt/hudson/NC_008253.fna
-vp-add-dataset -o --tag-name=clovr_mapping_bowtie_noindices_input_reads /opt/hudson/e_coli_1000.fq
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_paired_reads_1 /opt/hudson/rna_seq_data/test1.txt /opt/hudson/rna_seq_data/test2.txt
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_indices_paired_reads_2 /opt/hudson/rna_seq_data/testb1.txt /opt/hudson/rna_seq_data/testb2.txt
+vp-add-dataset -o --tag-name=clovr_mapping_bowtie_noindices_ref /opt/hudson/rna_seq_data/gasalab49.fsa
 
 # Generate configuration file that will be used by the pipeline
 vp-describe-protocols --config-from-protocol=clovr_mapping_bowtie_noindices \
-    -c input.REFERENCE_TAG=clovr_mapping_bowtie_noindices_reference \
-    -c input.INPUT_READS_TAG=clovr_mapping_bowtie_noindices_input_reads \
-    -c pipeline.PIPELINE_NAME=clovr_mapping_bowtie_noindices_${DATE} \
-    -c params.BOWTIE_BUILD_OPTS="-t 8" \
+    -c input.REFERENCE_TAG=clovr_mapping_bowtie_noindices_ref \
+    -c input.INPUT_PAIRED_TAG="clovr_mapping_bowtie_indices_paired_reads_1,clovr_mapping_bowtie_indices_paired_reads_2" \
+    -c cluster.CLUSTER_NAME=$1 \
+    -c cluster.CLUSTER_CREDENTIAL=$2 \
     -c params.OUTPUT_PREFIX="e_coli" > /tmp/$$.pipeline.conf
+
+vp-run-pipeline --validate --pipeline-config /tmp/$$.pipeline.conf
 
 # Run pipeline, block on checking status and verify exit code indicates a successful run
 TASK_NAME=`vp-run-pipeline --print-task-name --pipeline-config=/tmp/$$.pipeline.conf`
 
 if [ "$?" == "1" ]; then
-    echo "runPipeline.py failed to run"
+    echo "vp-run-pipeline failed to run"
     exit 1
 fi
 
