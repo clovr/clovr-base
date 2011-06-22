1089a1f9af31c3f16abbac261cfe547b56ecf399
orbitz <orbitz@gmail.com>
2011-06-22 17:51:29 +0000

    Updating webservices
    Former-commit-id: 8a472e0fe54418934f6f7162176d4c6a0b4aa65d

--- hudson/hudson-scripts/webService.sh
+++ hudson/hudson-scripts/webService.sh
@@ -2,11 +2,11 @@
 set -e
 source /root/clovrEnv.sh
 
-makeWSUrl.py http://localhost/vappio/clusterInfo_ws.py -j '{"name": "local"}'  
-makeWSUrl.py http://localhost/vappio/listClusters_ws.py -j '{"name": "local"}'  
-makeWSUrl.py http://localhost/vappio/pipelineStatus_ws.py -j '{"name": "local", "pipelines": []}'  
-makeWSUrl.py http://localhost/vappio/queryTag_ws.py -j '{"name": "local"}'  
-makeWSUrl.py http://localhost/vappio/task_ws.py -j '{"name": "local"}'  
+makeWSUrl.py http://localhost/vappio/clusterInfo_ws.py -j '{"cluster": "local"}'  
+makeWSUrl.py http://localhost/vappio/listClusters_ws.py -j '{"cluster": "local"}'  
+makeWSUrl.py http://localhost/vappio/pipeline_list -j '{"cluster": "local"}'
+makeWSUrl.py http://localhost/vappio/tag_list -j '{"cluster": "local"}'  
+makeWSUrl.py http://localhost/vappio/task_ws.py -j '{"cluster": "local"}'  
 
 
 
