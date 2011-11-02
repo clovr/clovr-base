e0561f5ac374c0b843550d380eba61493c69d466
Dave Riley <daveriley@fake.com>
2011-11-02 21:06:21 +0000

    Changed the types supported by clovr_16S and clovr_metagenomics to clovr_16s_metadata_file and clovr_metagenomics_metadata_file. Also added noautoshutdown and no master execs to lgt_wrapper.
    Former-commit-id: a615fbe90541520341f06056a31f6242e39aa9fa

--- clovr_pipelines/workflow/project_saved_templates/clovr_lgt_wrapper/set_queue.sh
+++ clovr_pipelines/workflow/project_saved_templates/clovr_lgt_wrapper/set_queue.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -q -i /mnt/keys/devel1.pem root@$1 "echo echo \\\\\\\$SGE_ROOT/bin/\\\\\\\$ARCH/qconf -rattr queue slots 1 \\\\\\\$execq@\\\\\\\$myhostname  \>\> /opt/vappio-scripts/start_exec.sh >> /opt/vappio-scripts/cli/exec_user-data.tmpl"
+
+ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -q -i /mnt/keys/devel1.pem root@$1 "touch /var/vappio/runtime/noautoshutdown"
+
+ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o ConnectTimeout=31 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -q -i /mnt/keys/devel1.pem root@$1 "source /opt/vappio-scripts/vappio_config.sh; export execq SGE_ROOT ARCH HOSTNAME; \$SGE_ROOT/bin/\$ARCH/qconf -rattr queue slots 0 \$execq\@\$HOSTNAME"
