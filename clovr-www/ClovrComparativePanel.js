170136f41c0f66ee061a34addcd14af16f862f84
Dave Riley <daveriley@fake.com>
2013-05-01 15:26:05 +0000

    Added new clovr comparative interface javascript files. Forgot them last time.
    Former-commit-id: cf0c7f5d5c29e309677cf264d1d557ea06ca1a53

---
+++ clovr-www/ClovrComparativePanel.js
@@ -0,0 +1,366 @@
+/*
+ * A form panel that can take the parameter hash from the clovr web services
+ * and make a form out of it.
+ */
+
+clovr.ClovrFormPanel = Ext.extend(Ext.Container, {
+
+    constructor: function(config) {
+        var title = new Ext.Container({
+            height: '30px',
+            name: 'form_title',
+            style: {
+                'padding': '3px 0 2px 5px',
+                'vertical-align': 'baseline', 
+                'font-size': '16pt',
+                'font-family': 'Trebuchet MS,helvetica,sans-serif',
+                'line-height': '33px',
+                'background': 'url("/clovr/images/clovr-vm-header-bg-short.png") repeat-x scroll center top'
+            },
+            region: 'north',
+            html: config.protocol
+        });
+        var thispanel = this;
+        var clovrform = this.createForm();
+        config.form = clovrform
+        // Fields that won't be drawn
+        config.ignore_fields = [];
+        this.protocol = config.protocol;
+        // We'll use this field to store a reference to the field that is used for tag input.
+        this.tag_field = null;
+        
+        // Generate the input fields
+        var rets = this.createInputFields(config);
+        var input_fieldset = rets[0];
+        var genbank_fieldset = rets[1];
+        var itemsArray = [input_fieldset,genbank_fieldset];
+        
+        // Generate the cluster fields
+        var cluster_fieldset = this.createClusterFields(config);
+        itemsArray.push(cluster_fieldset);
+        
+        // Generate all the other fields
+        var other_fields = clovr.makeDefaultFieldsFromPipelineConfig(config.fields,config.ignore_fields);
+        // add the normal ones...
+        itemsArray.push(other_fields.normal);
+        
+        // add the advanced ones...
+        itemsArray.push(
+            {xtype: 'fieldset',
+             title: 'Advanced',
+             collapsible: true,
+             listeners: {
+                 afterlayout: {
+                    fn: function(set) {
+                        set.collapse();
+                    },
+                    single: true
+                 }
+             },
+             items: other_fields.advanced
+            });
+
+        // add the hidden ones
+        itemsArray.push(other_fields.hidden);
+        
+        // Add the fields to the form
+        clovrform.add(itemsArray);
+        
+        // Call the parent constructor.
+        clovr.ClovrFormPanel.superclass.constructor.call(this, Ext.apply(config, {
+            items: [title,clovrform],
+            buttonAlign: 'center',
+            style: {
+                padding: '0px'
+            },
+            autoScroll: true,
+//            frame: true,
+//            defaults: {frame: false},
+            style: {
+                background: '#0D5685'
+            },
+            defaultType: 'textfield'
+        }));
+    },
+    setInput: function(input_tag) {
+//        console.log(this.tag_field);
+//        this.getForm().setValues([{id: this.tag_field,
+//                                  value: input_tag}]);
+    },
+    validate: function() {
+        var panel = this;
+        var credential = this.form.getForm().findField('cluster.CLUSTER_CREDENTIAL').getValue();
+        var cluster_name = clovr.getClusterName({
+            protocol: this.protocol+'_',
+            credential: credential
+        });
+        var params = this.form.getForm().getValues();
+        Ext.apply(params,{'cluster.CLUSTER_NAME': cluster_name,
+                          'cluster.CLUSTER_CREDENTIAL': credential
+                          });
+        var genbank_ids = [];
+
+        var store = panel.selectedseqsgrid.getStore();
+
+        store.each(function(r,i,a) {
+            genbank_ids.push(r.id);
+        });
+
+        Ext.apply(params,{'input.GENBANK_IDS': genbank_ids.join(',')});
+        clovr.validatePipeline({
+            pipeline: 'clovr_wrapper',
+            wrappername: this.wrappername,
+            cluster: cluster_name,
+            params: params
+        });    
+    
+    },
+    run: function() {
+        var panel = this;
+        var credential = this.form.getForm().findField('cluster.CLUSTER_CREDENTIAL').getValue();
+        var cluster_name = clovr.getClusterName({
+            protocol: this.protocol+'_',
+            credential: credential
+        });
+	    var params = this.form.getForm().getValues();
+        Ext.apply(params,{'cluster.CLUSTER_NAME': cluster_name,
+                          'cluster.CLUSTER_CREDENTIAL': credential
+                          });
+        var genbank_ids = [];
+        panel.selectedseqsgrid.getStore().each(function(r,i,a) {
+            genbank_ids.push(r.id);
+        })
+            Ext.apply(params,{'input.GENBANK_IDS': genbank_ids.join(',')});
+            clovr.runPipeline({
+            pipeline: 'clovr_wrapper',
+            wrappername: this.wrappername,
+            cluster: cluster_name,
+            params: params,
+            submitcallback: function(r) {
+                panel.form.getForm().reset();
+                panel.submitcallback(r);         
+            }
+        });
+    
+    },
+    createClusterFields: function(config) {
+        var credential_combo = clovr.credentialCombo({
+            name: 'cluster.CLUSTER_CREDENTIAL',
+            default_value: config.default_credential,
+            hidden: config.hide_credential});
+/*        var cluster_combo = clovr.clusterCombo({
+            name: 'cluster.CLUSTER_NAME',
+            default_value: config.default_cluster,
+            hidden: config.hide_cluster
+        });*/
+        var cluster_fieldset = {
+            xtype: 'fieldset',
+            hideMode: 'visibility',
+            title: 'CLoVR Cluster Selection',
+            items: [credential_combo]};
+        config.ignore_fields['cluster.CLUSTER_CREDENTIAL'] = 1;
+        return cluster_fieldset;
+    
+    },
+    createInputFields: function(config) {
+        var thispanel = this;
+        var input_fieldset = {xtype: 'fieldset',
+             title: 'Input Data Sets',
+        };
+        var genbank_fieldset = {xtype: 'fieldset',
+            title: 'Select Genomes from RefSeq',
+//            layout: 'fit',
+            layout: 'anchor',
+            anchorSize: {height: 300}
+        };
+        var input_regexp = /^input.INPUT_/;
+        var tag_regex = /.*TAG$/;
+
+        var input_fields = [];
+        Ext.each(config.fields, function(field, i, fields) {
+            
+            // This is a HACK to detect fields that are tags/datasets.
+            if((field.type=='dataset' && field.visibility != 'default_hidden')) { //|| tag_regex.exec(field.name)) && field.visibilty != 'default_hidden') {
+                var tag_combo = clovr.tagCombo({
+                    fieldLabel: field.display,
+                    field: field,
+                    width: 225,
+                    triggerAction: 'all',
+                    mode: 'local',
+                    valueField: 'name',
+                    displayField: 'name',
+                    forceSelection: true,
+                    editable: false,
+//                    submitValue: false,
+                    lastQuery: '',
+                    allowBlank: ! field.require_value,
+                    listEmptyText: 'No available data of this type',
+                    //emptyText: 'Nothing selected',
+                    name: field.name,
+                    tpl: '<tpl for="."><div class="x-combo-list-item"><b>{name}</b><br/>Format: {[values["metadata.format_type"]]}</div></tpl>',
+                    filter: {
+                        fn: function(record) {
+                            var re_types = [];
+                            Ext.each(field.type_params, function(type,i,params) {
+                                if(type.format_type) {
+                                    re_types.push(type.format_type.join('|'));
+                                }
+                            });
+                            var restr = re_types.join('|');
+                            var re = new RegExp(restr);
+                            return re.test(record.data['metadata.format_type']);
+                        }
+                    },
+                    listeners: {
+                        select: function(combo,rec) {
+    //                        wrapper_panel.load_pipeline_subform(config.pipelines);
+                	    }
+                    },
+                    afterload: function() {
+                       	tag_combo.fireEvent('select');
+                    } 
+                });
+                input_fields.push(tag_combo);
+		config.ignore_fields[field.name] = 1;
+            }
+			else if(field.name=='input.GENBANK_IDS') {
+
+
+				config.ignore_fields[field.name] = 1;
+				var genbank_id_store = new Ext.data.JsonStore({
+					fields: ['orgName', 'refseqId', {
+						name : 'seqLen',
+						type : 'int'
+					}],
+					mode: 'local',
+					autoLoad: false,
+				});
+				var genbank_combo = new Ext.ux.form.SuperBoxSelect({
+					field: field,
+					mode: 'local',
+					editable: true,
+					returnString: true,
+					store: genbank_id_store
+				});
+				var genbank_field = new Ext.form.CompositeField({
+					fieldLabel: 'Genbank IDs to download',
+					msgTarget: 'under',
+					invalidClass: '',
+					items: [
+					    genbank_combo,
+					    {xtype: 'button',
+					    text: 'Select Sequences',
+						    handler: function() {
+						        thispanel.showComparativeTreeWindow({id_store: genbank_id_store});
+						   }
+					   }
+				   ]
+				});
+				var newpanel = new clovr.ClovrComparativeTreePanel({width: '500px',height: '300px'});
+				thispanel.selectedseqsgrid = newpanel.grid;
+				var container = new Ext.Container({
+				    anchor: '100%',
+				    layout: 'fit',
+				    height: 300,
+				    items: [newpanel]
+				});
+				genbank_fieldset.items = [container];
+//			    input_fields.push(container);
+			}
+            else if(field.type=='dataset list' && field.visibilty != 'default_hidden') {
+                config.ignore_fields[field.name] = 1;                
+                var tag_combo = clovr.tagSuperBoxSelect({
+                    fieldLabel: field.display,
+                    field: field,
+                    width: 225,
+                    triggerAction: 'all',
+                    mode: 'local',
+                    valueField: 'name',
+                    displayField: 'name',
+                    forceSelection: true,
+                    editable: false,
+                    returnString: true,
+//                    submitValue: false,
+                    lastQuery: '',
+                    allowBlank: false,
+                    name: field.name,
+                    tpl: '<tpl for="."><div class="x-combo-list-item"><b>{name}</b><br/>Format: {[values["metadata.format_type"]]}</div></tpl>',
+                    filter: {
+                        fn: function(record) {
+                            var re_types = [];
+                            Ext.each(field.type_params, function(type,i,params) {
+                                if(type.format_type) {
+                                    re_types.push(type.format_type.join('|'));
+                                }
+                            });
+                            var restr = re_types.join('|');
+                            var re = new RegExp(restr);
+                            return re.test(record.data['metadata.format_type']);
+                        }
+                    },
+                    listeners: {
+                        select: function(combo,rec) {
+    //                        wrapper_panel.load_pipeline_subform(config.pipelines);
+                	    }
+                    },
+                    afterload: function() {
+                       	tag_combo.fireEvent('select');
+                    } 
+                });
+                input_fields.push(tag_combo);
+            }            
+        });
+        
+        // add the input tag parameters to the input_fieldset
+        input_fieldset.items = [input_fields];
+        return [input_fieldset,genbank_fieldset];
+    },
+    createForm: function() {
+        var panel = this;
+        return new Ext.form.FormPanel({
+            region: 'center',
+            style: {
+                'padding': '3px 3px 3px 3px'
+            },
+            frame: true,
+//            defaults: {frame: true},
+            buttonAlign: 'center',
+            buttons: [{
+                text: 'Clear',
+                handler: function(b,e) {
+                    panel.form.getForm().reset();
+                }},{
+                text: 'Validate',
+                handler: function(b,e) {
+                    panel.validate();
+                }},{
+                text: 'Run',
+                handler: function(b,e) {
+                    panel.run();
+            }}]
+        });    
+    },
+    // Display a window for selection of genomes
+    showComparativeTreeWindow: function(config) {
+        var win = new Ext.Window({
+            layout: 'fit',
+            height: 400,
+            width: 600,
+            closeAction: 'hide',
+            autoScroll: true,
+            title: 'CLoVR Comparative RefSeq selection'
+        });
+
+        var panel = new clovr.ClovrComparativeTreePanel({
+            id_store: config.id_store,
+            callback: function() {
+                win.close();
+            }
+            });
+        win.add(panel);
+        win.show();
+    }
+
+});
+
+Ext.reg('clovrcomparativepanel', clovr.ClovrFormPanel);
---
+++ clovr-www/ClovrComparativeTreePanel.js
@@ -0,0 +1,363 @@
+/*
+ * A panel used to select genomes from a taxonomy.
+ */
+Ext.ns('clovr');
+
+clovr.ClovrComparativeTreePanel = Ext.extend(Ext.Panel, {
+    constructor: function(config) {
+        var container = this;
+//          var URL = 'http://driley-lx.igs.umaryland.edu:8080/cgi-bin/clovr_comparative/cgi-bin/';
+        var URL = 'http://cb2.igs.umaryland.edu/cgi-bin/clovr_comparative/cgi-bin/';
+        var treeLoader;
+        var treeStore;
+        var searchStore;
+        var gridStore;
+        // Config for the tree panel.
+        var treepanel;
+        var treepanelconf = {
+            region : 'west',
+            title : 'RefSeq Taxonomy',
+            width : 300,
+            autoScroll : true,
+            collapsible : true,
+            split : true,
+            frame: false,
+            defaults: {frame: false},
+            useArrows : true,
+                        ddGroup: 'gridDrop',
+                        dropConfig: {
+                            appendOnly: true
+                        },
+                        enableDD: true,
+            root : new Ext.tree.AsyncTreeNode({
+                text : 'Root Node',
+                id : '/'
+            }),
+            listeners: {
+                load: function() {
+                    treepanel.getRootNode().expand();
+                    treepanel.getRootNode().firstChild.expand();
+                    treepanel.getRootNode().firstChild.expandChildNodes(false);
+                }
+            },
+            rootVisible : false,
+        };
+        
+        if(config.local_data_url) {
+            // Create a store for the tree.
+            treeLoader = new Ext.tree.TreeLoader({ 
+                dataUrl: '../cgi-bin/NcbiJsonSender.cgi'
+            });
+            treeStore = new Ext.data.JsonStore({
+                url : '../cgi-bin/SendGridJson.cgi',
+                root : 'info',
+                fields : ['orgName', 'refseqId', {
+                    name : 'seqLen',
+                    type : 'int'
+                }]
+            });
+            
+            // Create a store for the search combobox
+            searchStore = new Ext.data.ArrayStore({
+                url : '../cgi-bin/GetLineage.cgi',
+                id : 0,
+                fields : ['nodeName']
+            });
+            gridStore = new Ext.data.JsonStore({
+                root: 'info',
+                fields: ['orgName', 'refseqId', {
+                    name : 'seqLen',
+                    type : 'int'
+                }],
+                url: '../SendGridJson.cgi'
+            }); 
+        // If we are going to use a remote data source we'll go in here.
+        }
+        else {
+
+            if(config.remote_data_url) {
+                URL = config.remote_data_url;
+            }
+            // Below is a version of the tree loader that uses ScriptTagProxy instead of a normal http request.
+            treeLoader = new Ext.tree.TreeLoader({
+                directFn:function(nodeId,callback){
+                    var tree = treepanel;//Ext.getCmp('tree-container');
+                    container.getEl().mask('Loading Taxonomic Tree...', 'x-mask-loading');
+
+                    // Create a store to read in the results from the proxy.
+                    var store = new Ext.data.JsonStore({
+                        autoLoad: false,
+                        fields: ['id','text','children'],
+                        proxy: new Ext.data.ScriptTagProxy({
+                            url: URL+'/NcbiJsonSender.cgi'
+                        })
+                    });
+
+                    store.load({
+                        callback: function() {
+                            var response = [];
+                            // Like to find a better way to do this.
+                            for(i=0;i<store.data.items.length;i++){
+                                response.push(store.data.items[i].data);
+                            }
+                            callback(response, {status:true});
+                            container.getEl().unmask();
+                         },
+                         params: {node:nodeId}
+                   });
+                }
+            });
+            treeStore = new Ext.data.JsonStore({
+                root: 'info',
+                autoLoad: false,
+                fields: ['orgName', 'refseqId', {
+                            name : 'seqLen',
+                            type : 'int'
+                    }],
+                proxy: new Ext.data.ScriptTagProxy({
+                    url: URL+'/SendGridJson.cgi'
+                })
+            });
+            
+            // Below is a version of the tree store that uses ScriptTagProxy instead of a normal http request.
+            searchStore =  new Ext.data.ArrayStore({
+                proxy: new Ext.data.ScriptTagProxy({
+                    url : URL+'/GetLineage.cgi'
+                }),
+                id : 0,
+                fields : ['nodeName']
+            }); 
+            gridStore = new Ext.data.JsonStore({
+                root: 'info',
+                idProperty: 'refseqId',
+                fields: ['orgName', 'refseqId', {
+                    name : 'seqLen',
+                    type : 'int'
+                }],
+                proxy: new Ext.data.ScriptTagProxy({
+                    url: 'http://driley-lx.igs.umaryland.edu:8080/cgi-bin/clovr_comparative/cgi-bin/SendGridJson.cgi',
+                    listeners: {'load' : function(proxy,obj,opts) {
+//                              console.log(obj.info);
+//                              gridStore.loadData(obj.info,true);
+//                              return false;
+                        }}
+                })
+            }); 
+        }
+        
+        // Need to setup some beforeload stuffs.
+//          treeLoader.on("beforeload", function(treeLoader,node){
+//              treeLoader.baseParams.checked = node.attributes.checked;
+//          });
+          
+        // A combobox for searching the tree.
+        var searchCombo = new Ext.form.ComboBox({
+            xtype : 'combobox',
+            width : 300,
+            store: searchStore,
+            emptyText : 'Type a node name to auto display',
+            displayField : 'nodeName',
+            valueField : 'nodeName',
+            editable : true,
+            lazyRender : true,
+            mode : 'remote',
+            forceSelection : true,
+            triggerAction : 'all',
+            typeAhead : true,
+            typeAheadDelay : 300, 
+        });
+  
+        if(config.local_data_url) {
+            searchCombo.on('select', function() {
+                Ext.Ajax.request({
+                    url : '../cgi-bin/GetLineage.cgi',
+                    method : 'POST',
+                    params : {
+                        selectedNode : searchCombo.getValue()
+                    },
+                    success : function(response) {
+                        var treeContainer = treepanel; //Ext.getCmp('tree-container');                
+                        var array = Ext.util.JSON.decode(response.responseText);
+                        var node;
+                        Ext.each(array, function(item, index, allItems) {
+                            node = treeContainer.getNodeById(item);
+                            node.expand();
+                        });
+                        node.select();
+                        //node.fireEvent('click');
+                        gridStore.load({
+                            params : {
+                                id : node.attributes.id
+                            },add: true
+                        });
+                    }
+                });
+        
+            });
+        }
+        else {
+            searchCombo.on('select', function() {
+                var dumbstore = new Ext.data.ArrayStore({
+                    proxy: new Ext.data.ScriptTagProxy({
+                        url: URL+'/GetLineage.cgi'
+                    }),
+                    fields: ['name'],
+                    listeners: {
+                        load: function() {
+                            var treeContainer = treepanel; //Ext.getCmp('tree-container');
+                            var node;
+                            Ext.each(dumbstore.data.items, function(item, index, allItems) {
+                                node = treeContainer.getNodeById(item.json);
+                                node.expand();
+                            });
+                            node.select();
+                            treeStore.load({
+                                params : {
+                                    id : node.attributes.id
+                                }
+                            });
+                        }
+                    
+                    }});
+                    dumbstore.load({params : {selectedNode: searchCombo.getValue()}});
+            })
+        }      
+    
+    var grid_actions = new Ext.ux.grid.RowActions({
+                id: 'grid_actions',
+                keepselection: true,
+                actions:[{
+                                iconCls:'bin_closed',
+                                tooltip:'Remove this entry'
+                        }]
+        });
+        grid_actions.on(
+            'action',
+            function(grid, record, action, row, col) {
+                if(action == 'bin_closed') {
+                    grid.getStore().remove(record);
+                }
+            }); 
+    var selectedGrid = new Ext.grid.GridPanel({
+        title : 'Selected sequences',
+        store : gridStore,
+        form: true,
+        viewConfig: {
+            emptyText: 'Drag and drop Genomes from the tree on the left. Any level in the tree can be brought over!',
+            deferEmptyText: false
+        },
+        region: 'center',
+        plugins: [grid_actions],
+        height : 500,
+        autoExpandColumn: 'org-name',
+                ddGroup: 'gridDrop',
+                enableDragDrop: true,
+        //width : 800,
+        columns : [ new Ext.grid.RowNumberer(), {
+            header : 'ID',
+            width : 30,
+            dataIndex : 'id',
+            sortable : true,
+            hidden : true
+        }, {
+            id : 'org-name',
+            header : 'Organism name', 
+            width : 100,
+            dataIndex : 'orgName',
+            sortable : true
+        }, {
+            header : 'Seq. Length',
+            width : 90,
+            dataIndex : 'seqLen',
+            sortable : true
+        }, {
+            header : 'Refseq ID',
+            width : 100,
+            dataIndex : 'refseqId',
+            sortable : true,
+            align : 'center',
+            renderer: function(val) {
+                return "<a href='http://www.ncbi.nlm.nih.gov/sites/entrez?db=genome&cmd=search&term="+
+                    val+"' target='_blank'>"+val+"</a>";
+            }
+        },grid_actions],
+        autoExpandColumn : 'org-name',
+        loadMask : true,
+        columnLines : true,
+        buttonAlign: 'center',
+        buttons: [/*{
+            text: 'Save',
+            handler: function() {
+                var ids = [];
+//                  gridStore.each(function(item) {
+//                      console.log(item);
+//                      ids.push(item.data.refseqId);
+//                  });
+                console.log(config);
+                if(config.id_store) {
+                    console.log('here with an id store');
+                    console.log(gridStore);
+//                      config.id_store.loadData(gridStore.data,true);
+                    gridStore.removeAll();
+                    if(config.callback) {
+                        config.callback();
+                    }
+                }
+//                  console.log(ids);
+            }
+        },*/
+        {text: 'Clear',
+        handler: function() {
+            gridStore.removeAll();
+        }}
+
+        ]
+//        bbar : myBar
+    });
+
+    selectedGrid.on('render' , function() {
+        var gridel = selectedGrid.getView().scroller.dom;
+        if(config.id_store) {
+//              gridStore.loadData(config.id_store.data);
+        }
+        var gridtarget = new Ext.dd.DropTarget(gridel, {
+        ddGroup: 'gridDrop',
+            notifyDrop: function(ddSource, e, data) {
+                if(data.node) {
+                    gridStore.load({
+                        params : {
+                            id : data.node.id
+                        },
+                        add: true
+                    });
+                }
+                Ext.each(data.nodes, function(node,i,all) {
+                    gridStore.load({
+                        params : {
+                            id : node.id
+                        },
+                        add: true
+                    });
+        });
+        }
+
+    });
+
+    });
+    treepanelconf.tbar = [searchCombo];
+    treepanelconf.store = treeStore;
+    treepanelconf.loader = treeLoader;
+//      treepanel = new Ext.ux.MultiSelectTreePanel(treepanelconf);
+    treepanel = new Ext.tree.TreePanel(treepanelconf);
+    searchStore.load();
+    var treeconfig = {
+        layout : 'border',
+        defaults: {frame: false},
+        frame: false,
+        items: [selectedGrid,treepanel]
+    };
+    clovr.ClovrComparativeTreePanel.superclass.constructor.call(this, treeconfig);
+    this.grid = selectedGrid;
+}});
+
+Ext.reg('clovrcomparativetreepanel', clovr.ClovrComparativeTreePanel);
